82bd9a68e0c831665b78b522aa19accd7dde01c4
nataliescottdavidson <davidsonnatalies@gmail.com>
2020-09-20 16:26:49 -0400

    Bringing walkdir back

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,29 @@ fn printentry(entry: &DirEntry) {
     println!("{:?}", entry)
 }
 
-fn main() -> io::Result<()> {
-    let test_path = Path::new("test_dir");
-    visit_dirs(test_path, &printentry)?;
-    Ok(())
+fn create_override(directory: &std::path::Path) -> ignore::overrides::Override {
+    let mut override_builder = ignore::overrides::OverrideBuilder::new(directory);
+    let glob_override = override_builder.build().unwrap();
+    glob_override
+}
+
+fn print_dir() {
+    let test_dir = "test_dir";
+    let test_path = std::path::PathBuf::from(&test_dir);
+    let orrde = create_override(&test_path);
+    let files: Vec<_> = ignore::WalkBuilder::new(test_dir)
+        .git_ignore(false)
+        .hidden(false)
+        .build()
+        .map(|entry| entry.unwrap().path().to_owned())
+        .collect();
+
+    for i in files {
+        println!("File {:?}", &i)
+    }
+
+}
+
+fn main() {
+    print_dir();
 }
